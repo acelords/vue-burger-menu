6d9003ae9b2d8677241bd0ad106d13550d7a3c7b
Lexx YungCarter <spencermainaa@gmail.com>
2020-06-25 17:14:13 +0300

    feat: Added slots to slide component

--- src/components/Menu/slide.vue
+++ src/components/Menu/slide.vue
@@ -2,6 +2,18 @@
     <div>
         <Menu v-bind="this.$attrs" @openMenu="openMenu" @closeMenu="closeMenu">
             <slot></slot>
+
+            <template v-slot:button>
+                <slot name="button">
+                    <span class="bm-burger-bars line-style" :style="{top:20 * (index * 2) + '%'}" v-for="(x, index) in 3" :key="index"></span>
+                </slot>
+            </template>
+
+            <template v-slot:close>
+                <slot name="close">
+                    <span v-for="(x, index) in 2" :key="x" class="bm-cross" :style="{ position: 'absolute', width: '3px', height: '14px',transform: index === 1 ? 'rotate(45deg)' : 'rotate(-45deg)'}"></span>
+                </slot>
+            </template>
         </Menu>
     </div>
 </template>
